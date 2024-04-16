95a7b3a20915dcf4b3c0529d7e40666461a2310d
artem schegortzov <tema.chegortzov@mail.ru>
2024-04-16 17:03:49 +0300

    some changes in PrimaryButton component
    added isPassive mode

--- Frontend/src/components/UI/Buttons/PrimaryButton.jsx
+++ Frontend/src/components/UI/Buttons/PrimaryButton.jsx
@@ -2,11 +2,20 @@ import Button from './Button'
 
 import styles from './PrimaryButton.module.css'
 
-function PrimaryButton({ title, buttonClassName, onClick = () => {} }) {
+function PrimaryButton({
+  title,
+  buttonClassName,
+  isPassive,
+  onClick = () => {},
+}) {
   return (
     <Button
       title={title}
-      buttonClassName={[styles.primaryButton, buttonClassName].join(' ')}
+      buttonClassName={[
+        styles.primaryButton,
+        buttonClassName,
+        isPassive && styles.primaryButton_passive,
+      ].join(' ')}
       onClick={onClick}
     />
   )
