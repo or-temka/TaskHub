a74dda918d44aa6ba30dcb3b714e6e26fbb279c9
artem schegortzov <tema.chegortzov@mail.ru>
2024-04-16 13:54:06 +0300

    created PrimaryButton component

--- Frontend/src/components/UI/Buttons/PrimaryButton.jsx
+++ Frontend/src/components/UI/Buttons/PrimaryButton.jsx
@@ -1,7 +1,15 @@
+import Button from './Button'
 
+import styles from './PrimaryButton.module.css'
 
-function PrimaryButton() {
-  return <div>PrimaryButton</div>
+function PrimaryButton({ title, buttonClassName, onClick = () => {} }) {
+  return (
+    <Button
+      title={title}
+      buttonClassName={[styles.primaryButton, buttonClassName].join(' ')}
+      onClick={onClick}
+    />
+  )
 }
 
 export default PrimaryButton
