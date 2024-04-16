3ed29ec29ec90be3209bfd27059f7c861270e1a8
artem schegortzov <tema.chegortzov@mail.ru>
2024-04-16 11:48:05 +0300

    some changes in header component

--- Frontend/src/components/header/StudentHeader.jsx
+++ Frontend/src/components/header/StudentHeader.jsx
@@ -1,8 +1,11 @@
 import ButtonWithIcon from '../UI/Buttons/ButtonWithIcon'
 import Header from './Header'
+import { ReactComponent as TasksIcon } from '../../assets/svg/tasks.svg'
 
 function StudentHeader({ pageName }) {
-  const menuButtons = [<ButtonWithIcon />, <ButtonWithIcon />]
+  const menuButtons = [
+    <ButtonWithIcon title="Задания" svgElem={<TasksIcon />} />,
+  ]
 
   return <Header profileId={1} menuButtons={menuButtons} pageName={pageName} />
 }
