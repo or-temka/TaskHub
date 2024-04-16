702aac9b0aa1e9dca7845ec745a12e87483ce977
artem schegortzov <tema.chegortzov@mail.ru>
2024-04-16 12:10:13 +0300

    added menu buttons for teacher

--- Frontend/src/components/header/TeacherHeader.jsx
+++ Frontend/src/components/header/TeacherHeader.jsx
@@ -1,8 +1,14 @@
 import Header from './Header'
 import ButtonWithIcon from '../UI/Buttons/ButtonWithIcon'
+import { ReactComponent as TasksIcon } from '../../assets/svg/tasks.svg'
+import { ReactComponent as GroupIcon } from '../../assets/svg/group-of-people.svg'
 
 function TeacherHeader({ pageName }) {
-  const menuButtons = [<ButtonWithIcon />, <ButtonWithIcon />]
+  const menuButtons = [
+    <ButtonWithIcon title="Задания" svgElem={<TasksIcon />} />,
+    <ButtonWithIcon title="Группы" svgElem={<GroupIcon />} />,
+    <ButtonWithIcon title="Студенты" svgElem={<GroupIcon />} />,
+  ]
 
   return <Header menuButtons={menuButtons} pageName={pageName} />
 }
