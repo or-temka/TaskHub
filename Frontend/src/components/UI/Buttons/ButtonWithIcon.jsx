f1c8352358e5a3bbc0ddf40f9213dd5ef80f6469
artem schegortzov <tema.chegortzov@mail.ru>
2024-04-16 13:54:27 +0300

    updated ButtonWithIcon component

--- Frontend/src/components/UI/Buttons/ButtonWithIcon.jsx
+++ Frontend/src/components/UI/Buttons/ButtonWithIcon.jsx
@@ -1,9 +1,21 @@
 import buttonStyle from './Button.module.css'
 import styles from './ButtonWithIcon.module.css'
 
-function ButtonWithIcon({ title, svgElem }) {
+function ButtonWithIcon({
+  title,
+  svgElem,
+  buttonClassName,
+  onClick = () => {},
+}) {
   return (
-    <button className={[buttonStyle.button, styles.buttonWithIcon].join(' ')}>
+    <button
+      className={[
+        buttonStyle.button,
+        styles.buttonWithIcon,
+        buttonClassName,
+      ].join(' ')}
+      onClick={onClick}
+    >
       {svgElem}
       <span>{title}</span>
     </button>
