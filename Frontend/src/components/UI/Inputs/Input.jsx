fc68571d2b6761d093a63942122d3cc09144d9d7
artem schegortzov <tema.chegortzov@mail.ru>
2024-04-16 18:01:28 +0300

    added Input component

--- Frontend/src/components/UI/Inputs/Input.jsx
+++ Frontend/src/components/UI/Inputs/Input.jsx
@@ -1,7 +1,23 @@
+import styles from './Input.module.css'
 
-
-function Input() {
-  return <div>Input</div>
+function Input({
+  placeholder = '',
+  value = '',
+  onChange = () => {},
+  onClick = () => {},
+  className,
+  style,
+}) {
+  return (
+    <input
+      value={value}
+      onChange={onChange}
+      onClick={onClick}
+      placeholder={placeholder}
+      className={[styles.input, className].join(' ')}
+      style={style}
+    />
+  )
 }
 
 export default Input
