4e4c1da8c0eeb79e0b73fc5b93a11c7feff39ad5
artem schegortzov <tema.chegortzov@mail.ru>
2024-04-16 12:19:06 +0300

    added student, teacher navigation via menu buttons

--- Frontend/src/components/header/StudentHeader.jsx
+++ Frontend/src/components/header/StudentHeader.jsx
@@ -1,10 +1,13 @@
 import ButtonWithIcon from '../UI/Buttons/ButtonWithIcon'
 import Header from './Header'
 import { ReactComponent as TasksIcon } from '../../assets/svg/tasks.svg'
+import { Link } from 'react-router-dom'
 
 function StudentHeader({ pageName }) {
   const menuButtons = [
-    <ButtonWithIcon title="Задания" svgElem={<TasksIcon />} />,
+    <Link to="./">
+      <ButtonWithIcon title="Задания" svgElem={<TasksIcon />} />
+    </Link>,
   ]
 
   return <Header profileId={1} menuButtons={menuButtons} pageName={pageName} />
--- Frontend/src/components/header/TeacherHeader.jsx
+++ Frontend/src/components/header/TeacherHeader.jsx
@@ -2,12 +2,19 @@ import Header from './Header'
 import ButtonWithIcon from '../UI/Buttons/ButtonWithIcon'
 import { ReactComponent as TasksIcon } from '../../assets/svg/tasks.svg'
 import { ReactComponent as GroupIcon } from '../../assets/svg/group-of-people.svg'
+import { Link } from 'react-router-dom'
 
 function TeacherHeader({ pageName }) {
   const menuButtons = [
-    <ButtonWithIcon title="Задания" svgElem={<TasksIcon />} />,
-    <ButtonWithIcon title="Группы" svgElem={<GroupIcon />} />,
-    <ButtonWithIcon title="Студенты" svgElem={<GroupIcon />} />,
+    <Link to="./tasks">
+      <ButtonWithIcon title="Задания" svgElem={<TasksIcon />} />
+    </Link>,
+    <Link to="./groups">
+      <ButtonWithIcon title="Группы" svgElem={<GroupIcon />} />
+    </Link>,
+    <Link to="./">
+      <ButtonWithIcon title="Студенты" svgElem={<GroupIcon />} />
+    </Link>,
   ]
 
   return <Header menuButtons={menuButtons} pageName={pageName} />
