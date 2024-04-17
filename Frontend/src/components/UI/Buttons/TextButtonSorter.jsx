abc6b993c6926222250d90fddcd7f265065cf87c
artem schegortzov <tema.chegortzov@mail.ru>
2024-04-17 11:37:46 +0300

    created TextButtonSorter component

---
+++ Frontend/src/components/UI/Buttons/TextButtonSorter.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react'
+import styles from './TextButtonSorter.module.css'
+
+function TextButtonSorter({
+  className,
+  textClassName,
+  sortUpText = 'А-Я',
+  sortDownText = 'Я-А',
+  onClick = () => {},
+  userStatus = 'passive',
+  text,
+  ...params
+}) {
+  const [status, setStatus] = useState(userStatus)
+  const [sortText, setSortText] = useState('')
+
+  useEffect(() => {
+    setSortText(
+      status === 'passive' ? '' : status === 'up' ? sortUpText : sortDownText
+    )
+  }, [status])
+
+  const changeSortValueHandler = () => {
+    onClick()
+    setStatus(status === 'passive' ? 'up' : status === 'up' ? 'down' : 'up')
+  }
+
+  return (
+    <div
+      {...params}
+      className={[
+        styles.textButtonSorter,
+        className,
+        status !== 'passive' && styles.textButtonSorter_active,
+      ].join(' ')}
+      onClick={changeSortValueHandler}
+      data-sort-value={status}
+    >
+      <span
+        className={[
+          styles.textButtonSorter__text,
+          textClassName,
+          status !== 'passive' && styles.textButtonSorter__text_active,
+        ].join(' ')}
+      >
+        {sortText ? `${text} (${sortText})` : text}
+      </span>
+    </div>
+  )
+}
+
+export default TextButtonSorter
