6b4fed118ab9fa897d7ed8c1792b1ce07a2a3ca9
artem schegortzov <tema.chegortzov@mail.ru>
2024-04-17 15:45:33 +0300

    added PopUp component
    PopUp with your content

--- Frontend/src/components/UI/PopUps/PopUp.jsx
+++ Frontend/src/components/UI/PopUps/PopUp.jsx
@@ -1,5 +1,35 @@
-function PopUp() {
-  return <div>PopUp</div>
+import styles from './PopUp.module.scss'
+import PopUpContainer from './wrappers/PopUpContainer'
+
+function PopUp({
+  children,
+  headerText = '',
+  className,
+  headerClassName,
+  headerTextClassName,
+  contentClassName,
+  ...params
+}) {
+  return (
+    <PopUpContainer {...params} className={[styles.popUp, className].join(' ')}>
+      {headerText && (
+        <>
+          <header
+            className={[styles.popUp__header, 'text', headerClassName].join(
+              ' '
+            )}
+          >
+            <span className={['text', headerTextClassName].join(' ')}>
+              {headerText}
+            </span>
+          </header>
+        </>
+      )}
+      <main className={[styles.popUp__content, contentClassName].join(' ')}>
+        {children}
+      </main>
+    </PopUpContainer>
+  )
 }
 
 export default PopUp
