522704b65aaeccd27a6d0420dcc9a15052e28203
artem schegortzov <tema.chegortzov@mail.ru>
2024-04-17 15:44:50 +0300

    added PopUpSkeleton component
    wrapper for other PopUps

---
+++ Frontend/src/components/UI/PopUps/wrappers/PopUpSkeleton.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import styles from './PopUpSkeleton.module.scss'
+import CloseButton from '../../Buttons/CloseButton'
+
+function PopUpSkeleton({
+  children,
+  onClickBack = () => {},
+  onClickCloseButton,
+  isShowCloseButton = true,
+  className,
+  backClassName,
+  contentClassName,
+  ...params
+}) {
+  const [visiblePopUp, setVisiblePopUp] = useState(true)
+
+  if (!onClickCloseButton) {
+    onClickCloseButton = () => setVisiblePopUp(false)
+  }
+
+  if (!visiblePopUp) {
+    return
+  }
+  return (
+    <div {...params} className={[styles.popUpSkeleton, className].join(' ')}>
+      <div
+        className={[styles.popUpSkeleton__back, backClassName].join(' ')}
+        onClick={onClickBack}
+      ></div>
+      <div
+        className={[styles.popUpSkeleton__content, contentClassName].join(' ')}
+      >
+        {children}
+        {isShowCloseButton && (
+          <CloseButton
+            className={styles.popUpSkeleton__closeButton}
+            onClick={onClickCloseButton}
+          />
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default PopUpSkeleton
