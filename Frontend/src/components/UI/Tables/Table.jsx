2846f82eecd6fdb65230ae92775ab4047c741321
artem schegortzov <tema.chegortzov@mail.ru>
2024-04-18 12:58:29 +0300

    added Table component

---
+++ Frontend/src/components/UI/Tables/Table.jsx
@@ -0,0 +1,49 @@
+import { v4 as uuidv4 } from 'uuid'
+
+import styles from './Table.module.scss'
+
+function Table({
+  ths = [],
+  trsOfTds = [[]],
+  className,
+  trClassName,
+  thClassName,
+  tdClassName,
+  ...params
+}) {
+  return (
+    <table {...params} className={[styles.table, className].join(' ')}>
+      <thead className={styles.table__thead}>
+        <tr className={[styles.table__tr, trClassName].join(' ')}>
+          {ths.map((th) => (
+            <th
+              className={[styles.table__th, thClassName].join(' ')}
+              key={uuidv4()}
+            >
+              {th}
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody className={styles.table__tbody}>
+        {trsOfTds.map((tr) => (
+          <tr
+            className={[styles.table__tr, trClassName].join(' ')}
+            key={uuidv4()}
+          >
+            {tr.map((td) => (
+              <td
+                className={[styles.table__td, tdClassName].join(' ')}
+                key={uuidv4()}
+              >
+                {td}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
+
+export default Table
