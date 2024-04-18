89d6d11fa955d3bdb5001b617b9d27dec63ed9e0
artem schegortzov <tema.chegortzov@mail.ru>
2024-04-18 17:18:54 +0300

    done Tasks page for student

--- Frontend/src/pages/student/Tasks.jsx
+++ Frontend/src/pages/student/Tasks.jsx
@@ -1,7 +1,25 @@
-function Tasks({ setPageName }) {
-  setPageName('Задания')
+import ContentHeader from '../../components/frames/ContentHeader'
+import SmallTaskInfo from '../../components/student/SmallTaskInfo'
 
-  return <div>Tasks</div>
+import styles from './Tasks.module.scss'
+
+import { userTasks } from '../../data/userTasks'
+
+const PAGE_NAME = 'Задания'
+
+function Tasks({ setPageName }) {
+  setPageName(PAGE_NAME)
+
+  return (
+    <div className={['wrapper', styles.tasks].join(' ')}>
+      <ContentHeader title={PAGE_NAME}></ContentHeader>
+      <div className={styles.tasks__tasks}>
+        {userTasks.map((task) => (
+          <SmallTaskInfo task={task} key={task.id} />
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Tasks
