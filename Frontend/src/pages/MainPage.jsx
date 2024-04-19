caa881dd688f24f847c6ddbcd454cb288040c776
artem schegortzov <tema.chegortzov@mail.ru>
2024-04-19 08:00:43 +0300

    added MainPage page content

--- Frontend/src/pages/MainPage.jsx
+++ Frontend/src/pages/MainPage.jsx
@@ -1,7 +1,15 @@
+import SpinLoader from '../components/UI/Loaders/SpinLoader'
+
+import styles from './MainPage.module.scss'
+
 function MainPage({ setPageName }) {
   setPageName('')
 
-  return <div>MainPage</div>
+  return (
+    <div className={styles.mainPage}>
+      <SpinLoader />
+    </div>
+  )
 }
 
 export default MainPage
