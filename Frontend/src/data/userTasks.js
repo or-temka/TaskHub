edf07d0b054b637ce2a601262327b362dd2d96a3
artem schegortzov <tema.chegortzov@mail.ru>
2024-04-20 19:21:49 +0300

    added svg to-arrow and updated userTasks

--- Frontend/src/data/userTasks.js
+++ Frontend/src/data/userTasks.js
@@ -2,10 +2,16 @@ export const userTasks = [
   {
     id: 1,
     originalTaskId: 1,
-    mark: 3,
+    mark: 4,
+    notRoundMark: 3.6,
     newTask: false,
     dateCreate: '12 марта в 10:00',
     attemptsCount: 2,
+    statistics: {
+      taskRuntime: 560,
+      avarageQuestionTime: 15,
+      trueAnswersCount: 4,
+    },
   },
   {
     id: 2,
