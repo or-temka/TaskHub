5eb4b54d3fbf2d7b77d4bd256c98f37419acd921
artem schegortzov <tema.chegortzov@mail.ru>
2024-04-20 19:23:29 +0300

    added MyTaskStatistics component

---
+++ Frontend/src/components/student/MyTaskStatistics.jsx
@@ -0,0 +1,53 @@
+import getTimeExecuteInfo from '../../utils/getTimeExecuteInfo'
+import ContentContainer from '../frames/ContentContainer'
+import styles from './MyTaskStatistics.module.scss'
+import TaskAdoptStatisticLine from './TaskAdoptStatisticLine'
+
+function MyTaskStatistics({ userTask, questionsCount, className, ...params }) {
+  const taskStatistics = userTask.statistics
+
+  return (
+    <ContentContainer
+      {...params}
+      className={[styles.myTaskStatistics, className].join(' ')}
+    >
+      <div className={styles.myTaskStatistics__linesWithStatistics}>
+        <TaskAdoptStatisticLine
+          title="Балл за задание:"
+          value={userTask.notRoundMark}
+          valueChangeTo={userTask.mark}
+          valueBackgroundColor={
+            userTask.notRoundMark >= 4
+              ? 'var(--success-light-color)'
+              : userTask.notRoundMark >= 3
+              ? 'var(--warning-color)'
+              : 'var(--error-light-color)'
+          }
+          valueChangeToBackgroundColor={
+            userTask.mark >= 4
+              ? 'var(--success-light-color)'
+              : userTask.mark >= 3
+              ? 'var(--warning-color)'
+              : 'var(--error-light-color)'
+          }
+          adoptInfo="(оценка округлена)"
+        />
+        <TaskAdoptStatisticLine
+          title="Время выполнения задания:"
+          value={getTimeExecuteInfo(taskStatistics.taskRuntime)}
+        />
+        <TaskAdoptStatisticLine
+          title="Среднее время ответа на вопрос:"
+          value={getTimeExecuteInfo(taskStatistics.avarageQuestionTime)}
+        />
+        <TaskAdoptStatisticLine
+          title="Количество верных ответов:"
+          value={taskStatistics.trueAnswersCount}
+          outOfValue={questionsCount}
+        />
+      </div>
+    </ContentContainer>
+  )
+}
+
+export default MyTaskStatistics
