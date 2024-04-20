77480dab6cd86b06a53d6d654df41e2612bf0c6e
artem schegortzov <tema.chegortzov@mail.ru>
2024-04-20 19:22:14 +0300

    added Button component disabled mode

--- Frontend/src/components/UI/Buttons/Button.jsx
+++ Frontend/src/components/UI/Buttons/Button.jsx
@@ -1,10 +1,10 @@
 import styles from './Button.module.scss'
 
-function Button({ title = '', className, onClick = () => {}, ...params }) {
+function Button({ title = '', disabled, className, onClick = () => {}, ...params }) {
   return (
     <button
       {...params}
-      className={[styles.button, className].join(' ')}
+      className={[styles.button, className, disabled && styles.button_disabled].join(' ')}
       onClick={onClick}
     >
       {title}
