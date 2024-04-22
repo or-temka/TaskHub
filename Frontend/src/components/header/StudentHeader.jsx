3b510751237b3e70c6ebd2afad9ba1b3b1da3d0c
artem schegortzov <tema.chegortzov@mail.ru>
2024-04-22 14:24:57 +0300

    some changes in Header, StudentHeader

--- Frontend/src/components/header/StudentHeader.jsx
+++ Frontend/src/components/header/StudentHeader.jsx
@@ -1,16 +1,44 @@
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
+
 import ButtonWithIcon from '../UI/Buttons/ButtonWithIcon'
 import Header from './Header'
 import { ReactComponent as TasksIcon } from '../../assets/svg/tasks.svg'
-import { Link } from 'react-router-dom'
+import UserProfile from '../student/UserProfile'
+
+import users from '../../data/users'
+import groups from '../../data/groups'
 
 function StudentHeader({ pageName }) {
+  const [showProfile, setShowProfile] = useState(false)
+
+  const profileId = 1
+  const user = users.find((user) => user.id === profileId)
+  const userGroupName = groups.find((group) => group.id === user.groupId).name
+
   const menuButtons = [
     <Link to="./">
       <ButtonWithIcon title="Задания" svgElem={<TasksIcon />} />
     </Link>,
   ]
 
-  return <Header profileId={1} menuButtons={menuButtons} pageName={pageName} />
+  return (
+    <>
+      <Header
+        user={user}
+        menuButtons={menuButtons}
+        pageName={pageName}
+        onClickProfileButton={() => setShowProfile(true)}
+      />
+      {showProfile && (
+        <UserProfile
+          user={user}
+          userGroupName={userGroupName}
+          onCancel={() => setShowProfile(false)}
+        />
+      )}
+    </>
+  )
 }
 
 export default StudentHeader
