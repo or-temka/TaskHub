1f0308a85dd0728ce08792f92140b9c149ba8e51
artem schegortzov <tema.chegortzov@mail.ru>
2024-04-22 20:02:44 +0300

    some updates

--- Frontend/src/components/student/SmallTaskInfo.jsx
+++ Frontend/src/components/student/SmallTaskInfo.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+
 import ContentContainer from '../frames/ContentContainer'
 import ColorTextMsg from '../UI/Texts/ColorTextMsg'
 import Button from '../UI/Buttons/Button'
