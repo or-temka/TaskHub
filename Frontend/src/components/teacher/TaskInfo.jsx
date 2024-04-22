b8e422e468697a274702be0b088811eb6fcdd7c8
artem schegortzov <tema.chegortzov@mail.ru>
2024-04-22 20:03:15 +0300

    added TaskInfo component

---
+++ Frontend/src/components/teacher/TaskInfo.jsx
@@ -0,0 +1,107 @@
+import ContentContainer from '../frames/ContentContainer'
+import TextFocus from '../UI/Texts/TextFocus'
+import { ReactComponent as TaskSVG } from '../../assets/svg/tasks.svg'
+import File from '../UI/Files/File'
+
+import getTimeExecuteInfo from '../../utils/getTimeExecuteInfo'
+
+import styles from './TaskInfo.module.scss'
+
+function TaskInfo({ task, taskFiles, className, ...params }) {
+  return (
+    <ContentContainer
+      {...params}
+      className={[styles.taskInfo, className].join(' ')}
+    >
+      <header className={styles.taskInfo__header}>
+        <div className={styles.taskInfo__labelContainer}>
+          <TaskSVG />
+          <h6>Проверочная работа по теме "{task.name}"</h6>
+        </div>
+      </header>
+      <main className={styles.taskInfo__main}>
+        <div className={styles.taskInfo__infoContainer}>
+          <div>
+            <div className={styles.taskInfo__secondTaskInfo}>
+              <TextFocus className={styles.taskInfo__focusContainer}>
+                <span className={'text-bold'}>
+                  Начальное количество попыток:{' '}
+                </span>
+                <span
+                  className={[
+                    'text-bold',
+                    styles.taskInfo__attemptsCount,
+                    task.attempts === 0 && styles.taskInfo__attemptsCount_error,
+                  ].join(' ')}
+                >
+                  {task.attempts}
+                </span>
+              </TextFocus>
+              <TextFocus className={styles.taskInfo__focusContainer}>
+                <span
+                  className={['text-bold', styles.taskInfo__focusLabel].join(
+                    ' '
+                  )}
+                >
+                  Вопросов:{' '}
+                </span>
+                <span className={'text-bold'}>{task.questions.length}</span>
+              </TextFocus>
+              <TextFocus className={styles.taskInfo__focusContainer}>
+                <span
+                  className={['text-bold', styles.taskInfo__focusLabel].join(
+                    ' '
+                  )}
+                >
+                  Практических заданий:{' '}
+                </span>
+                <span className={'text-bold'}>
+                  {task.practiceQuestions.length}
+                </span>
+              </TextFocus>
+              <TextFocus
+                className={[
+                  styles.taskInfo__focusContainer,
+                  styles.taskInfo__timeForExecute,
+                ].join(' ')}
+              >
+                <span
+                  className={['text-bold', styles.taskInfo__focusLabel].join(
+                    ' '
+                  )}
+                >
+                  Времени на выполнение:{' '}
+                </span>
+                <span className={'text-bold'}>
+                  {getTimeExecuteInfo(task.timeForExecute)}
+                </span>
+              </TextFocus>
+            </div>
+          </div>
+          <TextFocus className={styles.taskInfo__focusContainer}>
+            <span className={'text-bold'}>Инструкции: </span>
+            <span className={'paragraph'}>{task.instruction}</span>
+          </TextFocus>
+          <TextFocus
+            className={[
+              styles.taskInfo__focusContainer,
+              styles.taskInfo__files,
+            ].join(' ')}
+          >
+            <span className={'text-bold'}>Справочный материал: </span>
+            {taskFiles.map((file) => (
+              <File
+                key={file.id}
+                extension={file.extension}
+                fileName={`${file.name}.${file.extension}`}
+                downloadLink={`/assets/files/${file.type}/${file.fileName}.${file.extension}`}
+              />
+            ))}
+          </TextFocus>
+        </div>
+      </main>
+    </ContentContainer>
+  )
+}
+
+export default TaskInfo
