edc1ab5855ea3745bd2753eac78a6a71b054e074
artem schegortzov <tema.chegortzov@mail.ru>
2024-04-22 15:07:27 +0300

    added TaskPerformHeader
    and included in TaskPerfrorm page

--- Frontend/src/pages/student/TaskPerform.jsx
+++ Frontend/src/pages/student/TaskPerform.jsx
@@ -1,11 +1,38 @@
 import { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+
+import TaskPerformHeader from '../../components/student/TaskPerformHeader'
+import ContentHeader from '../../components/frames/ContentHeader'
+
+import { userTasks } from '../../data/userTasks'
+import { tasks } from '../../data/tasks'
 
 function TaskPerform({ setPageName }) {
+  const taskId = +useParams().taskId
+
   useEffect(() => {
     setPageName('Выполнение задания')
   }, [])
 
-  return <div>TaskPerform</div>
+  const task = userTasks.find((task) => task.id === taskId)
+  const originalTask = tasks.find(
+    (originalTask) => originalTask.id === task.originalTaskId
+  )
+
+  useEffect(() => {
+    setPageName(`Выполнение задания "${originalTask.name}"`)
+  }, [])
+
+  return (
+    <>
+      <div className={'wrapper'}>
+        <ContentHeader
+          title={`Выполнение задания "${originalTask.name}"`}
+        ></ContentHeader>
+        <TaskPerformHeader task={task} originalTask={originalTask} />
+      </div>
+    </>
+  )
 }
 
 export default TaskPerform
