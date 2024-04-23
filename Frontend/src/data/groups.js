c457093116f9ec767076f84b4a27fb686d48a411
artem schegortzov <tema.chegortzov@mail.ru>
2024-04-23 09:02:39 +0300

    some updates

--- Frontend/src/data/groups.js
+++ Frontend/src/data/groups.js
@@ -4,6 +4,66 @@ const groups = [
     name: 'ЭИС-45',
     studentsId: [1],
   },
+  {
+    id: 2,
+    name: 'ЭИС-13',
+    studentsId: [],
+  },
+  {
+    id: 3,
+    name: 'ЭИС-14',
+    studentsId: [],
+  },
+  {
+    id: 4,
+    name: 'ЭИС-15',
+    studentsId: [],
+  },
+  {
+    id: 5,
+    name: 'ЭИС-16',
+    studentsId: [],
+  },
+  {
+    id: 6,
+    name: 'ЭПИ-19',
+    studentsId: [],
+  },
+  {
+    id: 7,
+    name: 'ЭИС-23',
+    studentsId: [],
+  },
+  {
+    id: 8,
+    name: 'ЭИС-24',
+    studentsId: [],
+  },
+  {
+    id: 9,
+    name: 'ЭИС-25',
+    studentsId: [],
+  },
+  {
+    id: 10,
+    name: 'ЭИС-26',
+    studentsId: [],
+  },
+  {
+    id: 11,
+    name: 'ЭИС-27',
+    studentsId: [],
+  },
+  {
+    id: 12,
+    name: 'ЭИС-28',
+    studentsId: [],
+  },
+  {
+    id: 13,
+    name: 'ЭИС-35',
+    studentsId: [],
+  },
 ]
 
 export default groups
