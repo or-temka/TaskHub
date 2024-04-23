ad34677040a1022cb3d3cc69059d220af3e9b4b7
artem schegortzov <tema.chegortzov@mail.ru>
2024-04-23 17:10:10 +0300

    added AddGroup for teacher

---
+++ Frontend/src/components/teacher/PopUps/AddGroup.jsx
@@ -0,0 +1,89 @@
+import { useState } from 'react'
+
+import PopUp from '../../UI/PopUps/PopUp'
+import Input from '../../UI/Inputs/Input'
+import PrimaryButton from '../../UI/Buttons/PrimaryButton'
+import Button from '../../UI/Buttons/Button'
+
+import styles from './AddGroup.module.scss'
+
+function AddGroup({
+  onAddGroup = () => {},
+  onCancel = () => {},
+  className,
+  ...params
+}) {
+  const [showNotificationPopUp, setShowNotificationPopUp] = useState(false)
+
+  const [groupName, setGroupName] = useState('')
+  const [cource, setCource] = useState('')
+
+  const [errorsInput, setErrorsInput] = useState({})
+
+  const addGroupHandler = () => {
+    const errors = {}
+    if (!groupName) errors.inputGroupName = 'Укажите название группы'
+    if (!cource) errors.inputCource = 'Укажите курс для группы'
+    setErrorsInput({ ...errors })
+
+    if (Object.keys(errors).length !== 0) return
+    // Если нет ошибок
+    // TODO отправка данных
+    setShowNotificationPopUp(true)
+  }
+
+  const closeNotificationPopUp = () => {
+    onAddGroup()
+  }
+
+  return (
+    <>
+      <PopUp
+        {...params}
+        headerText="Добавление группы"
+        contentClassName={[styles.addGroup, className].join(' ')}
+        onCancel={onCancel}
+      >
+        <div className={styles.addGroup__content}>
+          <div className={styles.addGroup__inputLine}>
+            <span className={styles.addGroup__inputLabel}>Название группы</span>
+            <Input
+              placeholder="Введите название группы"
+              value={groupName}
+              onChange={(e) => setGroupName(e.target.value)}
+              className={styles.addGroup__input}
+              errorText={errorsInput.inputGroupName}
+            />
+          </div>
+          <div className={styles.addGroup__inputLine}>
+            <span className={styles.addGroup__inputLabel}>Курс</span>
+            <Input
+              placeholder="Введите курс"
+              value={cource}
+              onChange={(e) => setCource(e.target.value)}
+              className={styles.addGroup__input}
+              errorText={errorsInput.inputCource}
+            />
+          </div>
+        </div>
+        <div className={styles.addGroup__buttons}>
+          <PrimaryButton title="Добавить группу" onClick={addGroupHandler} />
+          <Button title="Отмена" onClick={onCancel} />
+        </div>
+      </PopUp>
+
+      {/* Уведомление об успешной отправке данных */}
+      {showNotificationPopUp && (
+        <PopUp
+          headerText="Уведомление"
+          onCancel={closeNotificationPopUp}
+          onClickBack={closeNotificationPopUp}
+        >
+          <span>Группа была успешно добавлена!</span>
+        </PopUp>
+      )}
+    </>
+  )
+}
+
+export default AddGroup
