7e5519fe05f3150680d9ba107a99f61f2a6ff7f7
artem schegortzov <tema.chegortzov@mail.ru>
2024-04-25 15:46:46 +0300

    updated index.js
    added connected to DB

---
+++ Backend/index.js
@@ -0,0 +1,27 @@
+import { DB_LOGIN, DB_PASSWORD } from './PASSWORDS.js'
+
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+
+import { serverError, serverLog } from './utils/serverLog.js'
+
+mongoose
+  .connect(
+    `mongodb+srv://${DB_LOGIN}:${DB_PASSWORD}@cluster0.rouuumh.mongodb.net/taskhub?retryWrites=true&w=majority&appName=Cluster0`
+  )
+  .then(() => serverLog('connected to DB'))
+  .catch((err) => serverError('connected error to DB!!! Error: ' + err))
+
+const app = express()
+const PORT = 4000
+
+app.use(express.json())
+app.use(cors())
+
+app.listen(PORT, (err) => {
+  if (err) {
+    return console.log(err)
+  }
+  serverLog('Server link: http://localhost:' + PORT)
+})
