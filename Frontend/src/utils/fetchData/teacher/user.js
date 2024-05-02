392df8f39853677705e57e704484f8638da15d98
artem schegortzov <tema.chegortzov@mail.ru>
2024-05-02 11:40:53 +0300

    added some fetchs

--- Frontend/src/utils/fetchData/teacher/user.js
+++ Frontend/src/utils/fetchData/teacher/user.js
@@ -14,3 +14,60 @@ export const fetchUsers = async () => {
     console.log(error)
   }
 }
+
+export const fetchEditUser = async (userId, userData) => {
+  try {
+    const { data } = await axios.patch(
+      `/user/${userId}`,
+      {
+        name: userData.name,
+        login: userData.login,
+        password: userData.password,
+      },
+      {
+        headers: {
+          Authorization: await getUserToken(),
+        },
+      }
+    )
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const fetchAddStudentInGroup = async (userId, groupId) => {
+  try {
+    const { data } = await axios.patch(
+      `/group/add_user_in_group/${groupId}`,
+      {
+        userId,
+      },
+      {
+        headers: {
+          Authorization: await getUserToken(),
+        },
+      }
+    )
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const fetchRemoveStudentFromGroup = async (userId) => {
+  try {
+    const { data } = await axios.patch(
+      `/user/del_group_from_user/${userId}`,
+      {},
+      {
+        headers: {
+          Authorization: await getUserToken(),
+        },
+      }
+    )
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+}
